2c42db9ec0fbacfb91c372c2df95f95d98115d0c
andreea rosca <andreea rosca>
2016-03-16 23:06:08 +0200

    Created server

---
+++ server.js
@@ -0,0 +1,37 @@
+var http = require("http");
+
+var server = http.createServer(function (request, response) {
+
+	request.on('error', function(err) {
+	    console.error(err);
+	    response.statusCode = 400;
+	    response.end();
+  	});
+
+	response.on('error', function(err) {
+    	console.error(err);
+  	});
+
+  var method = request.method; //The request object is an instance of IncomingMessage
+	var url = request.url;
+	var headers = request.headers; //mai exista optiunea rawHeaders
+							//.headers returneaza obiect, rawHeaders returneaza txt
+							//rawHeaders include userAgent, deci daca il folosesti, userAgent va returna undefined
+	var userAgent = headers['user-agent'];
+
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+
+   var respBody = "Hello, lume !";
+   response.end(respBody);
+
+});
+
+server.listen(8081);
+
+exports.server = server;
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
