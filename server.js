476254160126e73149754e48c58e9e76c69a1f25
andreea rosca <andreea rosca>
2016-03-17 12:14:58 +0200

    Added travis.yml

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
 var http = require("http");
 
-var server = http.createServer(function (request, response) {
+var server = http.createServer(function(request, response) {
 
-	request.on('error', function(err) {
-	    console.error(err);
-	    response.statusCode = 400;
-	    response.end();
-  	});
+    request.on('error', function(err) {
+        console.error(err);
+        response.statusCode = 400;
+        response.end();
+    });
 
-	response.on('error', function(err) {
-    	console.error(err);
-  	});
+    response.on('error', function(err) {
+        console.error(err);
+    });
 
-  var method = request.method; //The request object is an instance of IncomingMessage
-	var url = request.url;
-	var headers = request.headers; //mai exista optiunea rawHeaders
-							//.headers returneaza obiect, rawHeaders returneaza txt
-							//rawHeaders include userAgent, deci daca il folosesti, userAgent va returna undefined
-	var userAgent = headers['user-agent'];
+    var method = request.method; //The request object is an instance of IncomingMessage
+    var url = request.url;
+    var headers = request.headers; //mai exista optiunea rawHeaders
+    //.headers returneaza obiect, rawHeaders returneaza txt
+    //rawHeaders include userAgent, deci daca il folosesti, userAgent va returna undefined
+    var userAgent = headers['user-agent'];
 
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
 
-   var respBody = "Hello, lume !";
-   response.end(respBody);
+    // Send the response body as "Hello World"
+    var methodChunk = 'METHOD: ' + method + '\n';
+    var urlChunk = 'URL: ' + url + '\n';
+    var headersChunk = 'HEADERS: ' + headers + '\n';
+    var userAgentChunk = 'USERAGENT: ' + userAgent + '\n';
+
+    var respBody = methodChunk + urlChunk  + headersChunk + userAgentChunk  +'\n' ;
+    response.end(respBody);
 
 });
 
